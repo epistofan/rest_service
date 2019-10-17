61fe629315808972405d19f9b61633bbbbb53e3b
epistofan <andrejs.kolokuckis@inbox.lv>
2019-10-17 13:52:08 +0300

    sample rest service

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Controller {
+
+
+
+    @GetMapping("/getWorkers")
+    public String getWorkers(){
+
+
+
+        return "test";
+
+    }
+
+}
